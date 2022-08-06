064a56d2e551ee566afc2a63843ad8053cc30ee0
freezrz <rongzezhang33@gmail.com>
2022-08-06 15:55:59 +0800

    update host

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(9m4i$tm^2b%%hnptu8-w5j^_c01a#^m)yk=kfw@@it8&1^_a1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-3-1-8-112.ap-southeast-1.compute.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-13-215-140-211.ap-southeast-1.compute.amazonaws.com']
 
 
 # Application definition
