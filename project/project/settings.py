e5b6d7ee3d6f5a5f52f15053484fc32c38c37d02
Angai <angaisathia@yahoo.com>
2022-09-17 16:56:27 +0800

    Updated Settinggs

--- project/project/settings.py
+++ project/project/settings.py
@@ -132,3 +132,47 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+###########################################################
+# added for SUS project
+
+# 1. X-XSS-Protection 
+# Default: False
+# If True, the SecurityMiddleware sets the X-XSS-Protection: 1; mode=block header 
+# on all  responses that do not already have it.
+SECURE_BROWSER_XSS_FILTER = True
+
+# 3. HTTP Strict Transport Security (HSTS)
+###SECURE_HSTS_SECONDS = 31536000 
+#Default: False
+# If True, the SecurityMiddleware adds the includeSubDomains directive to the HTTP Strict Transport Security header. 
+# It has no effect unless SECURE_HSTS_SECONDS is set to a non-zero value.
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# Default: 0
+# If set to a non-zero integer value, the SecurityMiddleware sets the 
+# HTTP Strict Transport Security header on all responses that do not already have it.
+
+# 4. X-Frame-Options
+# Default: 'SAMEORIGIN'
+# The default value for the X-Frame-Options header used by XFrameOptionsMiddleware. 
+# You can use this settings if you want
+# X_FRAME_OPTIONS = 'DENY'
+SECURE_CROSS_ORIGIN_OPENER_POLICY = "SAMEORIGIN"
+
+# 5. X-Content-Type-Options
+SECURE_CONTENT_TYPE_NOSNIFF = True
+# Default: False
+# If True, the SecurityMiddleware sets the X-Content-Type-Options: nosniff 
+# header on all responses that do not already have it.
+
+# 6. SESSION_COOKIE_HTTPONLY
+# Default: True
+# Whether to use HTTPOnly flag on the session cookie. If this is set to True, 
+# client-side JavaScript will not to be able to access the session cookie.
+SESSION_COOKIE_SECURE = False
+# Default: False
+# Whether to use a secure cookie for the session cookie. If this is set to True, 
+# the cookie will be marked as “secure,” which means browsers may ensure 
+# that the cookie is only sent under an HTTPS connection
+
+#########################################################
