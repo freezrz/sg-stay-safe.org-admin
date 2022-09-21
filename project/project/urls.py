fa9a0634209cf2c13dd8b460cdfea949a24f70ce
Xuanhui <e0224931@u.nus.edu>
2022-09-21 22:25:01 +0800

    add otp module to enable mfa

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django_otp.admin import OTPAdminSite
+  
+admin.site.__class__ = OTPAdminSite
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
