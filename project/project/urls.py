2d12ae7317bbb281ec1895c02dd62f0f1a40c722
Angai <angaisathia@yahoo.com>
2022-09-22 22:47:09 +0800

    changed url

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('staysafe/', admin.site.urls),
 ]
 
 admin.site.site_header = 'SG Stay Safe Admin Portal'
 admin.site.index_title = 'System Management'
 admin.site.site_title = 'Admin Portal'
+
